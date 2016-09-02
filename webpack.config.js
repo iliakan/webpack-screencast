1e4ab27ea25f1f6b5d3de1572eaad0e6ed237b10
Михайло Голубовський <lamerumiata@gmail.com>
2016-09-02 19:08:52 +0300

    Webpack 2.5 Простая сборка - Окружение, NODE_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+
 module.exports = {
   entry: './home',
   output: {
@@ -7,11 +10,18 @@ module.exports = {
     library: 'home'
   },
 
-  watch: true,
+  watch: NODE_ENV == 'development',
 
   watchOptions: { // Опция для ускорения пересборки
     aggregateTimeout: 100 // default: 300
   },
 
-  devtool: 'cheap-inline-module-source-map'
+  devtool: NODE_ENV == 'development' ? 'cheap-inline-module-source-map' : null,
+
+  plugins: [
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(NODE_ENV),
+      LANG:     JSON.stringify('ru')
+    })
+  ],
 };
--- welcome.js
+++ welcome.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = function(message) {
+
+  if (NODE_ENV == 'development') {
+    console.log(message);
+  }
+  
   alert(`Welcome ${message}`);
 };
