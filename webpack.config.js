9cabe5c226938a4b4902803617112afc649d9fcf
Михайло Голубовський <lamerumiata@gmail.com>
2016-09-05 14:19:57 +0300

    Webpack 3.2 Несколько скриптов - NoErrorsPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,7 @@ module.exports = {
   devtool: NODE_ENV == 'development' ? 'cheap-inline-module-source-map' : null,
 
   plugins: [
+    new webpack.NoErrorsPlugin(),
     // Определение переменных
     new webpack.DefinePlugin({
       NODE_ENV: JSON.stringify(NODE_ENV),
