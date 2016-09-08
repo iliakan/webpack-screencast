a10f76a9cdff418582cbe1e502da24551307faa5
Михайло Голубовський <lamerumiata@gmail.com>
2016-09-08 16:11:43 +0300

    Webpack 5.4 Внешние библиотеки - Старые скрипты: пути и imports/exports

---
+++ frontend/home.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// let old = require('imports?workSettings=>{delay:500}!exports?Work!old');
+let old = require('old');
+
+old();
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,29 @@
-// webpack --profile --display-modules --display-reasons
+// http://webpack.github.io/docs/shimming-modules.html#imports-loader
+// http://webpack.github.io/docs/shimming-modules.html#exports-loader
 
 'use strict';
 
 module.exports = {
     context: __dirname + '/frontend',
-    entry:   './app',
+    entry: './home',
 
     output: {
-        path:       __dirname + '/public',
-        filename:   "app.js"
+        path:       __dirname + '/public/',
+        filename:   'home.js'
     },
 
     module: {
         loaders: [{
-            test:    /\.js$/,
-            include: __dirname + '/frontend',
-            // exclude: /\/node_modules\//,
-            loader: 'babel',
-            query: {
-                presets: ['es2015'],
-                plugins: ['transform-runtime']
-            }
-        }],
+            test: /old.js$/,
+            loader: "imports?workSettings=>{delay:500}!exports?Work"
+        }]
+    },
 
-        noParse: /angular\/angular.js/
+    resolve: {
+        root: __dirname + '/vendor',
+        alias: {
+            old: 'old/dist/old'
+        }
     }
+
 };
